04c1f8f1c74c76d7e8b94d60c6b5823efc7b81f5
yeonjae02 <0yeonjae2@naver.com>
2023-06-30 01:08:04 +0900

    주석 수정

--- Chapter03/07. 여러 페이지 가져오기.py
+++ Chapter03/07. 여러 페이지 가져오기.py
@@ -5,7 +5,7 @@ import pyautogui
 keyword = pyautogui.prompt("검색어를 입력하세요>>>")
 lastpage = pyautogui.prompt("마지막 페이지 번호를 입력해주세요>>>")
 pageNum = 1
-for i in range(1, int(lastpage)*10, 10): # 1~3 페이지
+for i in range(1, int(lastpage)*10, 10):
     print(f"================={pageNum} 페이지 내용 출력=================")
     response =requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={keyword}&start={i}")
     html = response.text
